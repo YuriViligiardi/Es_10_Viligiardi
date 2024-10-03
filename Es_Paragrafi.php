aede2b362e49d87658a5f242e9963e3fbcdc0ce9
YuriViligiardi <168528006+YuriViligiardi@users.noreply.github.com>
2024-10-03 09:29:18 +0200

    Modificato font-size

--- Es_Paragrafi.php
+++ Es_Paragrafi.php
@@ -10,7 +10,7 @@
     $size = rand(1,11);
     $num = rand(1,6);
     for ($i=0; $i < $num ; $i++) { 
-        echo "<p style='font-size: $size;'>Ciao sono Yuri</p>";
+        echo "<p style='font-size: {$size}px;'>Ciao sono Yuri</p>";
         $size = $size +2;
     }
     ?>
